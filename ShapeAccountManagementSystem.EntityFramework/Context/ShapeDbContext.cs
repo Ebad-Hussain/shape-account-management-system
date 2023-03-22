088fdbfb9e11011d77b5d215d26e226e3d1e9294
Ebad-Hussain <ebbad2@gmail.com>
2023-03-22 16:07:40 +0500

    initial commit

---
+++ ShapeAccountManagementSystem.EntityFramework/Context/ShapeDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShapeAccountManagementSystem.EntityFramework.Entities;
+
+namespace ShapeAccountManagementSystem.EntityFramework.Context
+{
+    public class ShapeDbContext : DbContext
+    {
+        public ShapeDbContext() : base()
+        {
+
+        }
+        public ShapeDbContext(DbContextOptions<ShapeDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ ShapeAccountManagementSystem/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShapeAccountManagementSystem.Models.Receivables;
+
+namespace ShapeAccountManagementSystem.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        public UserController()
+        {
+
+        }
+
+        [HttpPost]
+        public void SignUp(SignUpReceivableDto input)
+        {
+
+        }
+
+    }
+}
